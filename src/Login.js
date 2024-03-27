1dbc3140a6aa94e7dcf1ea64cd61e4f37864f5be
dhaval-makwana <158137362+dhaval-makwana@users.noreply.github.com>
2024-03-27 23:12:16 +0530

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,63 +1,97 @@
 import React, { useEffect, useState } from "react";
-
-import { NavLink } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+
 const Login = () => {
+  const [user, setUser] = useState("");
+  const [paswordData, setPaswordData] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    const [user, setUser] = useState();
-    const [paswordData, setPaswordData] = useState();
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        let data = JSON.parse(localStorage.getItem('list'))
-
-        let updateData = data[data.length - 1];
-
-        localStorage.setItem("updateData", JSON.stringify(updateData));
-
-        let updated = JSON.parse(localStorage.getItem("updateData"))
-
-        const updatedlogin = [...data]
-        updatedlogin[updated] = {
-            user: updated.fullname,
-            paswordData: updated.password,
-        }
-        setUser(updated.fullname)
-        setPaswordData(updated.password)
-
-
-    }, [])
-
-    const loginBtn = () => {
-        let allData = JSON.parse(localStorage.getItem('list'))
-        console.log(allData, "allsssssssssssssssssss");
-        if (user, paswordData === "") {
-            alert("blank")
-        }
-        else {
-            navigate('/studentdata')
-        }
+  useEffect(() => {
+    const StudentList = JSON.parse(localStorage.getItem("list"));
+    if (StudentList && location.state?.fromRegister) {
+      setUser(location.state?.email);
+      setPaswordData(location.state?.password);
     }
-    return (<>
+  }, []);
 
-        <div className="container-fluid">
-            <form className="mx-auto">
-                <h1 className="text-center">Login</h1>
-                <div class="mb-2 mt-5">
-                    <label for="exampleInputEmail1" class="form-label">User Name</label>
-                    <input type="text" class="form-control" value={user} onChange={(e) => setUser(e.target.value)} id="exampleInputEmail1" aria-describedby="emailHelp" />
+  const CheckUser = () => {
+    const StudentList = JSON.parse(localStorage.getItem("list"));
+    if (StudentList.length <= 0) {
+      return false;
+    }
+    const findUser = StudentList.find(
+      (i) =>
+        i.email.trim().toLowerCase() === user.trim().toLowerCase() &&
+        i.password.trim().toLowerCase() === paswordData.trim().toLowerCase()
+    );
+    console.log(StudentList);
+    return !!findUser;
+  };
 
-                </div>
-                <div class="mb-2">
-                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" value={paswordData} onChange={(e) => setPaswordData(e.target.value)} id="exampleInputPassword1" />
-                    <NavLink id="emailHelp" className="form-text" to="/">Forget Password</NavLink>
-                </div>
-                <div class="mb-2 mt-5">
-                    <button type="submit" class="btn btn-primary" onClick={loginBtn} >Login</button>
-                </div>
-            </form>
-        </div>
-    </>)
-}
+  const loginBtn = (e) => {
+    e.preventDefault();
+    const elements = new FormData(e.currentTarget);
+    const email = elements.get("email");
+    const password = elements.get("password");
+    console.log(email);
+    if (email === "" && password === "") {
+      alert("please fill Email and password");
+    } else {
+      const result = CheckUser();
+      console.log(result);
+      if (result) {
+        setUser("");
+        setPaswordData("");
+        navigate("/studentdata");
+      } else {
+        alert("user not found");
+      }
+    }
+  };
+  return (
+    <>
+      <div className="container-fluid ">
+        <form className="mx-auto" onSubmit={loginBtn}>
+          <h1 className="text-center">Login</h1>
+          <div className="mb-2 mt-5">
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              className="form-control"
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              required
+            />
+          </div>
+          <div className="mb-2">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              className="form-control"
+              value={paswordData}
+              onChange={(e) => setPaswordData(e.target.value)}
+              id="exampleInputPassword1"
+              required
+            />
+          </div>
+          <div className="mb-2 mt-5">
+            <button type="submit" className="btn btn-primary">
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
 export default Login;
