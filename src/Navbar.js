19afda3336e666218912cf687224159571aac09d
dhaval-makwana <158137362+dhaval-makwana@users.noreply.github.com>
2024-03-27 16:56:53 +0530

    enhance navbar code

--- src/Navbar.js
+++ src/Navbar.js
@@ -10,7 +10,9 @@ const Navbar = () => {
                     <div className="col-10 mx-auto">
                         <nav className="navbar navbar-expand-lg navbar-light">
                             <div className="container-fluid">
-                                <NavLink className="navbar-brand" to="/">Student Managment Application </NavLink>
+                                <NavLink className="navbar-brand" to="/">
+                                    Student Managment Application
+                                </NavLink>
                                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                                     <span className="navbar-toggler-icon"></span>
                                 </button>
